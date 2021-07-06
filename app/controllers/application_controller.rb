e75a94dda736834f7bdf3dcf1914fc732ad2637a
Tabata Nobuya <qqkz2qf9k@kind.ocn.ne.jp>
2021-07-07 02:30:30 +0900

    final

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   add_flash_types :success, :info, :warning, :danger
   before_action :require_login
-  
+
   private
 
   def not_authenticated
