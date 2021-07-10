a42b84b9e45dacfbf52d2ddfed30ca6bc1d54698
Tabata Nobuya <qqkz2qf9k@kind.ocn.ne.jp>
2021-07-10 11:09:55 +0900

    掲示板作成機能

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_09_005935) do
+ActiveRecord::Schema.define(version: 2021_07_09_102237) do
 
   create_table "boards", force: :cascade do |t|
     t.string "title"
@@ -19,14 +19,6 @@ ActiveRecord::Schema.define(version: 2021_07_09_005935) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_boards_on_user_id"
-  end
-
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
